9c89fce7856d33c3414f65d253430cb52b011f60
Vinicius Centurion <43175480+vncenturion@users.noreply.github.com>
2021-12-10 22:46:02 -0300

    Update menu.sh

--- Projeto Final/menu.sh
+++ Projeto Final/menu.sh
@@ -1,20 +1,27 @@
-#!/bin/sh
+#!/bin/sh                                                                                      
   
 KEY=12345
 
-#aba PROCESSOS
-yad --plug=${KEY} --tabnum=1 --text="first tab with text" &> res1 &
+controle=1
 
-#aba RECURSOS
-lscpu | tr -s " " | cut -d ":" -f 1,2 | yad --plug=${KEY} --tabnum=2 --text="second tab" --list --no-selection --column=$"Parameter" --column=$"Value" &> res2 &
+#aba PROCESSOS                                                                                 
+#yad --plug=${KEY} --tabnum=1 --text="first tab with text"                             
+    ps -eo "%p;%y;%U;%C;%x;%c" | tr -d " " | grep -E ^[[:digit:]] | tr ";" "\n" | \
+        yad --plug=${KEY} --tabnum=1 --text="Processos atuais" --list --column="PID" --column="TTY" --column="USER" --column="%CPU"
+        --column="TIME" --column="COMMAND" &> res1 &
 
-#aba ESPECIFICAÇÕES
-eval exec yad --plug=${KEY} --tabnum=3 --image=drive-harddisk --text="Disk\ usage" --buttons-layout=end \
---multi-progress $(df -h | tail +2 | grep -v snap | \
-awk '{printf "--bar=\"<b>%s</b> (%s) [%s/%s]\" %s ", $6, $1, $3, $2, $5}') &> res3 &
-#yad --plug=${KEY} --tabnum=3 --text="blablabla" &> res3 &
+#aba RECURSOS                                                                                  
+    lscpu | head -24 | tr -s ' ' | tr ':' '\n' | \
+        yad --plug=${KEY} --tabnum=2 --text="second tab" --list --no-selection --column="Componente" --column="Descrição" &> res2 &
 
-#janela PRINCIPAL
-yad --notebook --center --width=800 --height=450 --title="Informações de processos" --text="$TXT" --button="aplicar filtro" --button="matar processo" --button="sair" --key=${KEY} --tab="Processos" --tab="Recursos" --tab="Especificações"
+#aba ESPECIFICAÇÕES                                                                            
+    eval exec yad --plug=${KEY} --tabnum=3 --image=drive-harddisk --text="Disk\ usage" --buttons-layout=end \
+        --multi-progress $(df -h | tail +2 | grep -v snap | \
+        awk '{printf "--bar=\"<b>%s</b> (%s) [%s/%s]\" %s ", $6, $1, $3, $2, $5}') &> res3 &
+
+#janela PRINCIPAL                                                                              
+    yad --notebook --center --width=800 --height=450 --title="Gerenciador de processos" --text="$TXT" \
+        --button="aplicar filtro" --button="matar processo" --button="sair" \
+        --key=${KEY} --tab="Processos" --tab="Recursos" --tab="Especificações"
 
 ipcrm -M ${KEY}
