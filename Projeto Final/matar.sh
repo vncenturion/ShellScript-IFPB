86e3255ba550f6fd43b876ad17da2b4e8c4feff8
Vinicius Centurion <43175480+vncenturion@users.noreply.github.com>
2021-12-21 19:37:24 -0300

    Update matar.sh

--- Projeto Final/matar.sh
+++ Projeto Final/matar.sh
@@ -1,1 +1,4 @@
-yad --center --width=500 --height=500 --title="matar processo"
+processo=$(yad --title="Matar processo" --center --form --text="Digite o PID do processo que deseja matar:" --field="PID: " --button="OK:0" --button="Cancelar:1" --width=600)
+kill ${processo:0:-1}
+
+#yad --center --width=500 --height=500 --title="matar processo"
